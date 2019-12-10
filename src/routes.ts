d690c03d60f0b840c157eafa460e672983044657
Matt Seddon <mattseddon@hotmail.com>
2019-12-10 15:21:53 +1100

    setup express and test using supertest

---
+++ src/routes.ts
@@ -0,0 +1,23 @@
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+
+import Movies from './api/Movies';
+
+const app = express();
+const movies = new Movies();
+
+app.use(bodyParser.json());
+
+app.post('/movies', (req: express.Request, res: express.Response) => {
+  res.json(movies.create(req.body));
+});
+
+app.get('/movies', (req: express.Request, res: express.Response) => {
+  res.json(movies.findMany());
+});
+
+app.get('/test', (req: express.Request, res: express.Response) => {
+  res.json({ message: 'pass!' });
+});
+
+module.exports = app;
---
+++ src/test/routes.spec.ts
@@ -0,0 +1,62 @@
+import * as app from '../routes';
+import * as supertest from 'supertest';
+const request = supertest(app);
+
+describe('GIVEN the test endpoint', () => {
+  const endpoint = '/test';
+  describe('WHEN we call asynchronously', () => {
+    it('THEN returns the correct response', async done => {
+      const response = await request.get(endpoint);
+
+      expect(response.status).toBe(200);
+      expect(response.body.message).toBe('pass!');
+
+      done();
+    });
+  });
+});
+
+describe('GIVEN two movies and the movies GET and POST endpoints', () => {
+  const endpoint = '/movies';
+  const originalMovie: object = {
+    name: 'Jaws',
+    rating: 9,
+  };
+
+  const sequelMovie: object = {
+    name: 'Jaws: The Revenge',
+    rating: 1.4,
+  };
+
+  describe('WHEN we POST the movie data to the endpoint', () => {
+    it('THEN accepts the data', async done => {
+      const originalMovieResponse = await request
+        .post(endpoint)
+        .send(originalMovie)
+        .set('Accept', 'application/json');
+
+      expect(originalMovieResponse.status).toBe(200);
+
+      const sequelMovieResponse = await request
+        .post(endpoint)
+        .send(sequelMovie)
+        .set('Accept', 'application/json');
+
+      expect(sequelMovieResponse.status).toBe(200);
+
+      done();
+    });
+  });
+
+  describe('WHEN we GET the data from the endpoint', () => {
+    it('THEN returns the data', async done => {
+      const response = await request.get(endpoint);
+
+      expect(response.body.length).toBe(2);
+      expect(response.body).toContainEqual(originalMovie);
+      expect(response.body).toContainEqual(sequelMovie);
+
+      done();
+    });
+  });
+});
